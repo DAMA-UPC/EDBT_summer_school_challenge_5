5be831898eef9e73cb36ac8bc6494e81f702245b
Arnau Prat <arnau.prat@gmail.com>
2015-09-02 12:32:44 +0200

    Reading persons directly from file

--- reorg.c
+++ reorg.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 int main( int argc, char** argv ) 
 {
 	if(argc != 1) {
@@ -21,22 +22,24 @@ int main( int argc, char** argv )
 	size_t persons_file_size = ftell (persons_file);
 	long num_persons = persons_file_size / sizeof(PersonIn);
 
-	PersonIn* persons_in  = (PersonIn*)malloc(persons_file_size);
+/*	PersonIn* persons_in  = (PersonIn*)malloc(persons_file_size);
 	if( !persons_in ) {
 		fprintf(stderr, "ERROR while creating persons array");
 		exit(1);
 	}
 	fread(persons_in, sizeof(PersonIn), num_persons, persons_file);
+	*/
 
 	int i = 0;
 	for( i = 0; i < num_persons; ++i) {
-		PersonIn* person = &persons_in[i];
+		PersonIn person;
+		fread(&person, sizeof(PersonIn), 1, persons_file);
 		if( i % 10000 == 0 ) {
-			printf("Read %d persons. This person id %lu\n", i, person->person_id);
+			printf("Read %d persons. This person id %lu\n", i, person.person_id);
 		}
 	}
 
-	free(persons_in);
+	//free(persons_in);
 	fclose(persons_file);
 	fclose(knows_file);
   return 0;
